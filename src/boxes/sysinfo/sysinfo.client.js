fc7b09fbc685d43b4a9be746434d6769db0292d2
MK <mkoch@eyecone.com>
2016-01-07 23:37:45 +0100

    more stats

--- src/boxes/sysinfo/sysinfo.client.js
+++ src/boxes/sysinfo/sysinfo.client.js
@@ -4,13 +4,26 @@ var os = require('os'),
     moment = require('moment');
 
 HTMLImports.whenReady(function () {
+    function humanFileSize(B, i) {
+        var e = i ? 1e3 : 1024;
+        if (Math.abs(B) < e) return B + " B";
+        var a = i ? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"] : ["KiB", "MiB", "GiB", "TiB", "PiB", "Ei‌​B", "ZiB", "YiB"],
+            t = -1;
+        do B /= e, ++t; while (Math.abs(B) >= e && t < a.length - 1);
+        return B.toFixed(1) + " " + a[t]
+    }
+
     Polymer({
         is: 'box-sysinfo',
 
         hostname: String,
         uptime: Number,
-        freemem: Number,
-
+        freemem: String,
+        totalmem: String,
+        mempercent: String,
+        loadavg: String,
+        platform: String,
+        
         properties: {
             posAbsolute: {
                 type: Boolean,
@@ -46,9 +59,27 @@ HTMLImports.whenReady(function () {
             var that = this;
 
             this.hostname = os.hostname();
+            this.platform = os.platform()+' '+os.arch()+' ('+os.release()+')';
+            console.log(this.platform);
             this.uptime = moment((moment.now() - (os.uptime() * 1000))).fromNow();
-            this.freemem = 
+
+            this.freemem = humanFileSize(os.freemem(), true);
+            this.totalmem = humanFileSize(os.totalmem(), true);
+            this.mempercent = ((os.freemem() / os.totalmem()) * 100).toFixed(2);
             
+            var load = os.loadavg();
+            this.loadavg = '1m[' + load[0].toFixed(2) + ']' + ' 5m[' + load[1].toFixed(2) + ']' + ' 15m[' + load[2].toFixed(2) + ']';
+
+            // frequent updates
+            setInterval(function () {
+                var load = os.loadavg();
+                that.loadavg = '1m[' + load[0].toFixed(2) + ']' + ' 5m[' + load[1].toFixed(2) + ']' + ' 15m[' + load[2].toFixed(2) + ']';
+
+                that.freemem = humanFileSize(os.freemem(), true);
+                that.totalmem = humanFileSize(os.totalmem(), true);
+                that.mempercent = ((os.freemem() / os.totalmem()) * 100).toFixed(2);
+            }, 1000 * 60);
+
             setInterval(function () {
                 that.uptime = moment((moment.now() - (os.uptime() * 1000))).fromNow();
             }, 25000);
